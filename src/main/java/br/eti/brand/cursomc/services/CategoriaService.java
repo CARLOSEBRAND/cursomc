493231097863ee420d5253fd6954f515f43d71ab
Carlos Brand <carlosebrand@gmail.com>
2018-10-28 23:05:53 -0300

    Criando repository e services para categoria

---
+++ src/main/java/br/eti/brand/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package br.eti.brand.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.eti.brand.cursomc.domain.Categoria;
+import br.eti.brand.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+		
+	}
+
+}
