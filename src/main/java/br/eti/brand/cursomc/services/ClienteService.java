155ac68693d89f575d853e404dd359bdea96e1f7
Carlos Brand <carlosebrand@gmail.com>
2018-11-08 23:30:24 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/eti/brand/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.eti.brand.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.eti.brand.cursomc.domain.Cliente;
+import br.eti.brand.cursomc.repositories.ClienteRepository;
+import br.eti.brand.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));		
+	}
+
+}
